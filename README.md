b464093993f8623c9fe70c230ce35eba83d42b96
lopers71 <developersdevelopers71@gmail.com>
2025-04-11 14:54:34 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# SimpleInvoice
+
+Web-based Invoice Generator + Client Management tool for Freelancers and Small Businesses.
---
+++ routes.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from config import Config
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    @app.route('/')
+    def home():
+        return render_template("index.html")
+    
+    return app
